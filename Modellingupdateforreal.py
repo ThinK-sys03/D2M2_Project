e13d2c59621b6df28a099bc593e95aa8fd2b51d2
ThinK-sys03 <82259456+ThinK-sys03@users.noreply.github.com>
2021-09-16 15:40:39 +0800

    Update Modellingupdateforreal.py

--- Modellingupdateforreal.py
+++ Modellingupdateforreal.py
@@ -913,7 +913,7 @@ if __name__ == '__main__':
     st = time.time()
     period = 92  # Simulation period
     simulationround = 5 # number of simulations
-    ini_seednumber = np.random.randint(low=10,high=16) # Initial infectious inidividual
+    ini_seednumber = np.random.randint(low=5,high=16) # Initial infectious inidividual
     R0 = 2.5 # Basic reproduction number 
     Epsilon = 2.9 # latent period for symptomatic cases
     Epsilon_asy = 6.2 # latent period for asymptomatic cases
